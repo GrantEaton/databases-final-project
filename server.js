24e91e83ef699826c3fa67356839f957929bddb3
Luke <lartnak@gmail.com>
2017-04-24 18:44:32 -0400

    setup server side rendering

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+const path = require('path');
 const express = require('express');
 
 let app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
 
-  res.send('success');
+  res.render('home', {
+
+    topics: ['General', 'Sports', 'Programming', 'Cooking']
+
+  });
 
 });
 
-let server = app.listen(8000);
+let server = app.listen(8000, () => {
+
+  console.log('server running at http://localhost:8000');
+
+});
